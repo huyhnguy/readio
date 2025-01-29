ea08ae5b8d611f9f8458e8a914b22fb39b2cd47d
Huy Nguyen <huy.h.nguy@gmail.com>
2025-01-28 19:33:49 -0600

    create login page and send data to backend

--- backend/src/controllers/exampleController.ts
+++ backend/src/controllers/exampleController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const getExample = (req: Request, res: Response): void => {
   res.json({ message: 'Hello from the backend!' });
 };
 
-export default { getExample };
+const postLogin = (req: Request, res: Response): void => {
+    console.log(req.body);
+    res.json({ message: 'Hello from the backend!' });
+  };
+
+export default { getExample, postLogin };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,5 +4,6 @@ import exampleController from '../controllers/exampleController';
 const router = Router();
 
 router.get('/', exampleController.getExample);
+router.post('/login', exampleController.postLogin);
 
 export default router;
